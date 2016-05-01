dc9fcb154f930cd2fd427af84fb9ad10dd735368
Chris Sumption <fridaysedge@users.noreply.github.com>
2016-05-01 13:44:23 -0700

    Delete welcome.php

--- views/welcome.php
+++
@@ -1,3 +0,0 @@
-<?php ?>
-<p>This is the endpoint URL for the <?php echo $teiFile; ?> REST API.</p>
-<p>You can view available API resources <a href="<?php echo $url; ?>/api/v1/resources">here</a>.</p>
