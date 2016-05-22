b1497dcd336ac51eacc7ac1d21fdaf1fb4632d89
Chris Sumption <fridausedge@gmail.com>
2016-05-22 12:49:30 -0700

    add the gpl text file and update commenting related to the API

--- router.php
+++ router.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * router.php processes all URI requests for the API.
+ * Copyright (C) 2016 Chris Sumption, Michael Andrea, Guiyan Bai
+ *	
+ * This file is part of the Apitext API.
+ *
+ * The the Apitext API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The the Apitext API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 include("view.php");
 
 // Import configuration settings.
